31548c78411ae2989f33c90fddc4772f0dd67c56
Yeloda <guillaume.lamboley@yahoo.com>
2024-05-19 21:43:54 +0200

    image upload

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
+import { Toaster } from '@/components/ui/toaster'
 import React from 'react'
 
 const Layout = ({ children }: {children: React.ReactNode}) => {
@@ -17,6 +18,8 @@ const Layout = ({ children }: {children: React.ReactNode}) => {
                     {children}
                 </div>
             </div>
+
+            <Toaster />
         </main>
     )
 }
--- middleware.ts
+++ middleware.ts
@@ -13,5 +13,6 @@ export default clerkMiddleware((auth, req) => {
   });
 
 export const config = {
-  matcher: ["/((?!.*\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+//   matcher: ["/((?!.*\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
